1df133bb5f0014bab5d0b2a97d25caa2e82165f0
jagreehal <jag.reehal@gmail.com>
2018-11-04 22:23:22 +0000

    fixed path

--- astronaut-finder/handler.js
+++ astronaut-finder/handler.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 module.exports = async (context, callback) => {
     const result = await axios.get('http://api.open-notify.org/astros.json');
-    const data = result.data;
+    const data = result.data.people;
     const astro = data[Math.floor(Math.random() * data.length) + 1]
 
     callback(undefined, {
